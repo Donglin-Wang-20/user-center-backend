6eb4be93679f4c2e8bef5d47f9ca5e8baa18be8b
wangdonglin <donglinwang77@gmail.com>
2025-11-12 14:14:28 +0800

    feat：完成注册功能

--- src/main/java/com/yupi/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/yupi/usercenter/service/impl/UserServiceImpl.java
@@ -1,20 +1,84 @@
 package com.yupi.usercenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yupi.usercenter.model.domain.User;
 import com.yupi.usercenter.service.UserService;
 import com.yupi.usercenter.mapper.UserMapper;
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 /**
-* @author edy
-* @description 针对表【user】的数据库操作Service实现
-* @createDate 2025-11-11 15:49:14
-*/
+ * 用户服务实现类
+ *
+ * @author WangDonglin
+ */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
-    implements UserService{
+        implements UserService {
 
+    @Resource
+    private UserMapper userMapper;
+
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户id
+     */
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+
+        // 1. 校验非空
+        if (StringUtils.isAllBlank(userAccount, userPassword, checkPassword)) {
+            return -1;
+        }
+        if (userAccount.length() < 4) {
+            return -1;
+        }
+        if (userPassword.length() < 8 || checkPassword.length() < 8) {
+            return -1;
+        }
+        // 账户不能包含特殊字符
+        String validPattern = "^[a-zA-Z0-9_]+$";
+
+        // matches() 是 Java String 类的方法，用于判断整个字符串是否完全匹配给定的正则表达式。
+        if (!userAccount.matches(validPattern)) {
+            return -1;
+        }
+
+        // 密码和校验密码相同
+        if (userPassword.equals(checkPassword)) {
+            return -1;
+        }
+
+        // 账户不能重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        Long count = userMapper.selectCount(queryWrapper);
+        // long count = this.count(queryWrapper);
+        if (count > 0) {
+            return -1;
+        }
+
+        // 2. 对密码进行md5加密
+        final String SALT = "WangDonglin";
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        // 3. 插入数据
+        User  user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        boolean saveResult = this.save(user);
+        if (!saveResult) {
+            return -1;
+        }
+
+        return 0;
+    }
 }
 
 
